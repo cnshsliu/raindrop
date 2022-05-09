619ef472779afc91e5f7ad875fa796ba358c0f99
cnshsliu <liukehong@gmail.com>
2022-05-09 21:20:30 +0800

    hello

--- src/database/redis.ts
+++ src/database/redis.ts
@@ -1,15 +1,13 @@
 "use strict";
-import Redis from "redis";
+import { createClient } from "redis";
 import RaindropConfig from "../secret/keep_secret";
 
-/**
- *
- * ## The connect string for the OpenShift env
- *
- */
-const connection_string = RaindropConfig.redis;
-//running locally - make sure you've started redis server
-const redisClient = Redis.createClient(connection_string);
-//redisClient.auth(process.env.REDIS_DB_PASSWORD);
+const redisClient = createClient({
+  url: RaindropConfig.redis.connectionString,
+});
+
+redisClient.on("error", (err) => {
+  console.log("Redis Client Error", err);
+});
 
 export default redisClient;
