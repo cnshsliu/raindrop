154bbb907d0c2f375eacf7d85c7f1b1e8823cdd1
cnshsliu <liukehong@gmail.com>
2022-05-11 18:51:59 +0800

    auto commit

--- src/auth/wishhouse-strategy.ts
+++ src/auth/wishhouse-strategy.ts
@@ -3,8 +3,10 @@ const config = {
   validate: async function (token, request, h) {
     console.log("Wishhouse strategy - validate ", token);
     let credentials: any = {};
-    let result = { isValid: false, credentials: credentials };
+    let result = {isValid: false, credentials: credentials};
     let user_id = "";
+
+    //在此处调用中台， using token
     let user = { _id: "chengaoyang", username: "陈高阳" };
     if (user) {
       result = {
@@ -22,8 +24,10 @@ const config = {
       console.log("Refreshed credentials from database failed, user not found");
     }
 
+    /** 这段用于判断是否在黑名单里，即便验证通过
     if (result.isValid) {
       //does redis have the token
+      //示例直接返回不在黑名单中
       let inblack = false;
       //oops - it's been blacklisted - sorry
       if (inblack) {
@@ -37,7 +41,7 @@ const config = {
     return result;
   },
 
-  isValidToken: (token) => token && token.indexOf("pizza") >= 0,
+  isValidToken: (token) => token && token.indexOf("yuanwangwu") >= 0,
 
   errorFunc: ({ errorType, message, scheme }) => ({
     errorType,
